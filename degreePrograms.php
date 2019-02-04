45e6573de3fb278fa664b4ebe7d1ee6c45ab9604
Casey Bates <cbates10@vols.utk.edu>
2019-02-03 20:30:08 -0600

    changed to reflect new database schema

--- degreePrograms.php
+++ degreePrograms.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "216.96.149.200";
-$database = "graduateformscentral";
-$sqlusername = "root";
-$sqlpassword = "Imbroglio3724!";
+$database = "formscentral";
+$sqlusername = "Casey3724";
+$sqlpassword = "Imbroglio3724";
 
 $mysqli = new mysqli($servername, $sqlusername, $sqlpassword, $database);
 
@@ -11,7 +11,7 @@ if($mysqli->connect_errno) {
 	die("Failed to connect to MYSQL: ($mysqli->connect_errno) $mysqli->connect_error");
 }
 
-$sql = "SELECT * FROM majoroptions ORDER BY optionSubject";
+$sql = "SELECT * FROM majoroptions ORDER BY subject";
 
 $result = $mysqli->query($sql);
 
@@ -19,11 +19,11 @@ $degrees = array();
 
 if($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		if(!isset($degrees[$row["optionSubject"]])) {
-			$degrees[$row["optionSubject"]] = new stdClass();
-			$degrees[$row["optionSubject"]]->option = array();
+		if(!isset($degrees[$row["subject"]])) {
+			$degrees[$row["subject"]] = new stdClass();
+			$degrees[$row["subject"]]->option = array();
 		} 
-		$degrees[$row["optionSubject"]]->option[$row["optionName"]] = $row["optionHours"];
+		$degrees[$row["subject"]]->option[$row["name"]] = $row["requiredHours"];
 	}
 }
 
