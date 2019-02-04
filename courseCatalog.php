7a139ad6db3c36501b8d4e7ff12cdab4a6a7acb3
Casey Bates <cbates10@vols.utk.edu>
2019-02-03 20:31:03 -0600

    changed to reflect new database schema

--- courseCatalog.php
+++ courseCatalog.php
@@ -1,33 +1,37 @@
 <?php
 
 $servername = "216.96.149.200";
-$database = "graduateformscentral";
-$sqlusername = "root";
-$sqlpassword = "Imbroglio3724!";
+$database = "formscentral";
+$sqlusername = "Casey3724";
+$sqlpassword = "Imbroglio3724";
 
-$mysqli = new mysqli($servername, $sqlusername, $sqlpassword, $database);
+$con = new mysqli($servername, $sqlusername, $sqlpassword, $database);
 
-if($mysqli->connect_errno) {
-	die("Failed to connect to MYSQL: ($mysqli->connect_errno) $mysqli->connect_error");
+$con = mysqli_connect($servername, $sqlusername, $sqlpassword, $database);
+
+if($con->connect_errno) {
+	die("Failed to connect to MYSQL: ($mysql->connect_errno) $mysqli->connect_error");
 }
 
 $sql = "SELECT * FROM courses";
 
-$result = $mysqli->query($sql);
+$result = $con->query($sql);
 
 $courses = array();
 
 if($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		$courses[$row["courseNumber"]] = new stdClass();
-		$courses[$row["courseNumber"]]->hours = $row["courseHours"];
-		$courses[$row["courseNumber"]]->name = $row["courseName"];
+		$courses[$row["idCourses"]] = new stdClass();
+		$courses[$row["idCourses"]]->number = $row["courseNumber"];
+		$courses[$row["idCourses"]]->hours = $row["courseHours"];
+		$courses[$row["idCourses"]]->name = $row["courseName"];
 	}
 }
 
-$mysqli->close();
+$con->close();
 
 $courses = json_encode($courses);
-echo $courses;
+echo ($courses);
+
 
 ?>
