2b858a13a108bd664c33b3e4931ce796a690ce33
Casey Bates <cbates10@vols.utk.edu>
2019-02-05 21:12:22 -0600

    now uses session information for set program to retrieve the degree information

--- requirements.php
+++ requirements.php
@@ -6,11 +6,13 @@ $sqlusername = "Casey3724";
 $sqlpassword = "Imbroglio3724";
 $mysqli = new mysqli($servername, $sqlusername, $sqlpassword, $database);
 
+session_start();
+
 if($mysqli->connect_errno) {
 	die("Failed to connect to MYSQL: ($mysqli->connect_errno) $mysqli->connect_error");
 }
 
-$degreeOption = sanitizeInput($argv[1]);
+$degreeOption = $_SESSION["idMajorOptions"];
 
 $sql = "SELECT idCourses FROM requiredcourses WHERE idMajorOptions = ?";
 $stmt = $mysqli->prepare($sql);
