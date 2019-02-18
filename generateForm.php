5c66832918d802d848e2466d6345ce07da95b124
Casey Bates <cbates10@vols.utk.edu>
2019-02-18 07:11:49 -0600

    small formatting change

--- generateForm.php
+++ generateForm.php
@@ -2,11 +2,9 @@
 	require 'pdfForm.php';
 
 	$data = array();
-	$courseCounter = 1;
 	
 	foreach($_POST as $key => $value) {
 		$data[$key] = $value;
-		
 	} 
 	
 	$pdf = new pdfForm("admission_to_candidacy.pdf", $data);
