ed6478e27ff4df00abafb42208f5703de2ca7c7a
Casey Bates <cbates10@vols.utk.edu>
2019-02-20 14:06:28 -0600

    supports adminstrative login

---
+++ scripts/authenticateUser.php
@@ -0,0 +1,54 @@
+<?php
+
+$servername = "216.96.149.200";
+$database = "formscentral";
+$sqlusername = "Casey3724";
+$sqlpassword = "Imbroglio3724";
+
+$mysqli = new mysqli($servername, $sqlusername, $sqlpassword, $database);
+
+if($mysqli->connect_errno) {
+	die("Failed to connect to MYSQL: ($mysqli->connect_errno) $mysqli->connect_error");
+}
+
+$subject = sanitizeInput("username");
+$option = sanitizeInput("password");
+
+$sql = "SELECT accountId FROM accounts WHERE username = ? AND password = ?";
+
+$stmt = $mysqli->prepare($sql);
+if(!$stmt) {
+	die("Could not prepare statement $mysqli->error");
+}
+if(!$stmt->bind_param("ss", $subject, $option)) {
+	die("Could not bind parameters $mysqli->error");
+}
+$rc = $stmt->execute();
+if(false === $rc) {
+	die("Could not execute statement $stmt->error");
+}
+
+$stmt->bind_result($accountId);
+
+if($stmt->fetch()) {
+	session_start();
+	$_SESSION["accountId"] = $accountId; 
+	echo "success";
+	$stmt->close();
+	$mysqli->close();
+	die();	
+} else {
+	$stmt->close();
+	$mysqli->close();
+	die();
+}
+
+function sanitizeInput($field) {
+	if(!isset($_POST[$field])) {
+		$data = "";
+	} else {
+		$data = trim($_POST[$field]);
+		$data = htmlspecialchars($data);
+	}
+	return $data;
+}
