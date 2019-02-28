298601ece7b6043e5d65e0a51afb5258c3669101
Casey Bates <cbates10@vols.utk.edu>
2019-02-27 20:11:14 -0600

    changes to align with new database schema

--- scripts/addCourse.php
+++ scripts/addCourse.php
@@ -1,9 +1,6 @@
 <?php
     
-    $servername = "216.96.149.200";
-    $database = "formscentral";
-    $sqlusername = "Casey3724";
-    $sqlpassword = "Imbroglio3724";
+	include 'DatabaseInfo.php';
     
     $con = new mysqli($servername, $sqlusername, $sqlpassword, $database);
     
@@ -11,17 +8,22 @@
         die("Failed to connect to MYSQL: ($mysqli->connect_errno) $mysqli->connect_error");
     }
     
-    $sql = "INSERT INTO courses (coursePrefix, courseNumber, courseHours, courseName)
-    VALUES (?,?,?,?)";
+    $sql = "INSERT INTO courses (coursePrefix, courseNumber, courseHoursLow, courseHoursHigh, courseName) VALUES (?,?,?,?,?);";
     
-    $stmt = mysqli_prepare($sql);
-    if(!stmt){
+    $stmt = $con->prepare($sql);
+    if(!$stmt){
         die("Could not bind parameters $mysqli->error");
     }
-    $stmt->bind_param("sss", $_POST['coursePrefix'], $_POST['courseNumber'], $_POST['courseHours'], $_POST['courseName']);
+    $stmt->bind_param("siiis", $_POST['coursePrefix'], $_POST['courseNumber'], $_POST['courseHoursLow'], $_POST['courseHoursHigh'], $_POST['courseName']);
     
-    $stmt->execute();
+	$rc = $stmt->execute();
     
+	if(false === $rc) {
+		die("Failure to execute statement $stmt->error");
+	}
+	
+	echo "Course Added Successfully";
+	
     $stmt->close();
     $con->close();
     
