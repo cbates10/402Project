a5d1454001bbc68b5722022551f6bd09492f99ca
Casey Bates <cbates10@vols.utk.edu>
2019-03-04 21:48:23 -0600

    added ability to set events when the maximum number of hours of a particular level is exceeded

--- scripts/Requirements.js
+++ scripts/Requirements.js
@@ -9,11 +9,44 @@ class Requirements {
 		this.requiredHours = 0;
 		this.currentHours = 0;
 		this.hoursAction = null;
+		this.hours400 = null;
+		this.hours400Action = null;
+		this.currentHours400 = 0;
+		this.hours500 = null;
+		this.hours500Action = null;
+		this.currentHours500 = 0;
+		this.hours600 = null;
+		this.hours600Action = null;
+		this.currentHours600 = 0;
+
+	}
+	
+	set400LevelHours(hours, hoursAction) {
+		this.hours400 = hours;
+		if(hoursAction) {
+			this.hours400Action = hoursAction;
+		}
+	}
+	
+	set500LevelHours(hours, hoursAction) {
+		this.hours500 = hours;
+		if(hoursAction) {
+			this.hours500Action = hoursAction;
+		}
+	}
+	
+	set600LevelHours(hours, hoursAction) {
+		this.hours600 = hours;
+		if(hoursAction) {
+			this.hours600Action = hoursAction;
+		}
 	}
 	
 	setRequiredHours(hours, hoursAction) {
 		this.requiredHours = hours;
-		this.hoursAction = hoursAction;
+		if(hoursAction) {
+			this.hoursAction = hoursAction;
+		}
 	}
 	
 	addRequiredCourse(courseId, courseAction) {
@@ -40,13 +73,36 @@ class Requirements {
 		this.validationExpressions = this.validationExpressions.filter((exp) => exp !== func);
 	}
 	
-	addCourse(course, hours) {
+	addCourse(course, hours, number) {
+		if(number < 500)  {
+			this.currentHours400 += hours;
+			if(this.hours400Action !== undefined) {
+				if(this.currentHours400 > this.hours400) {
+					this.hours400Action();
+				}
+			}
+		} else if(number < 600) {
+			this.currentHours500 += hours;
+			if(this.hours500Action !== undefined) {
+				if(this.currentHours500 > this.hours500) {
+					this.hours500Action();
+				}
+			}
+		} else {
+			this.currentHours600 += hours;
+			if(this.hours600Action !== undefined) {
+				if(this.currentHours600 > this.hours600) {
+					this.hours600Action();
+				}
+			}
+		}
 		if(this.courses[course] !== undefined) {
 			this.courses[course] = true;
 			if(this.courseActions[course] !== undefined) {
 				this.courseActions[course]();
 			}
 		}
+		this.currentHours += hours;
 		if(this.hoursAction) {
 			this.hoursAction(hours);
 		}
