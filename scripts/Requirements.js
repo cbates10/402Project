53c90bd2b431267107a9b11a5de1f50e36a9979a
Casey Bates <cbates10@vols.utk.edu>
2019-03-09 13:00:44 -0600

    Added functionality to reset notification events after removal events set course hours outside of event thresholds

--- scripts/Requirements.js
+++ scripts/Requirements.js
@@ -3,6 +3,7 @@ class Requirements {
 	constructor() {
 		this.courseActions = {};
 		this.coursesTaken = {};
+		this.courseRows = {};
 		this.requiredCourses = {};
 		this.subCourses = {};
 		this.fufilledOperation = function() { return false; }
@@ -13,12 +14,15 @@ class Requirements {
 		this.hoursAction = null;
 		this.hours400 = null;
 		this.hours400Action = null;
+		this.hours400Notification = false;
 		this.currentHours400 = 0;
 		this.hours500 = null;
 		this.hours500Action = null;
+		this.hours500Notification = false;
 		this.currentHours500 = 0;
 		this.hours600 = null;
 		this.hours600Action = null;
+		this.hours600Notification = false;
 		this.currentHours600 = 0;
 	}
 	
@@ -80,24 +84,28 @@ class Requirements {
 		if(catalogEntry.number < 500)  {
 			this.currentHours400 += hours;
 			if(this.hours400Action) {
-				var hourCap = (this.requiredHours > this.currentHours) ? (this.requiredHours * this.hours400) : (this.currentHours * this.hours400); 
-				if(this.currentHours400 > hourCap) {
+				var hourCap = (this.requiredHours > this.currentHours) ? (Math.ceil(this.requiredHours * this.hours400)) : (Math.ceil(this.currentHours * this.hours400));
+				console.log("The hour cap is " + hourCap);
+				if(this.currentHours400 > hourCap && !this.hours400Notification) {
+					this.hours400Notification = true;
 					this.hours400Action();
 				}
 			}
 		} else if(catalogEntry.number < 600) {
 			this.currentHours500 += hours;
 			if(this.hours500Action) {
-				var hourCap = (this.requiredHours > this.currentHours) ? (this.requiredHours * this.hours500) : (this.currentHours * this.hours500); 
-				if(this.currentHours500 >  hourCap) {
+				var hourCap = (this.requiredHours > this.currentHours) ? (Math.ceil(this.requiredHours * this.hours500)) : (Math.ceil(this.currentHours * this.hours500)); 
+				if(this.currentHours500 > hourCap && !this.hours500Notification) {
+					this.hours500Notification = true;
 					this.hours500Action();
 				}
 			}
 		} else {
 			this.currentHours600 += hours;
 			if(this.hours600Action) {
-				var hourCap = (this.requiredHours > this.currentHours) ? (this.requiredHours * this.hours600) : (this.currentHours * this.hours600); 
-				if(this.currentHours600 > hourCap) {
+				var hourCap = (this.requiredHours > this.currentHours) ? (Math.ceil(this.requiredHours * this.hours600)) : (Math.ceil(this.currentHours * this.hours600)); 
+				if(this.currentHours600 > hourCap && !this.hours600Notification) {
+					this.hours600Notification = true;
 					this.hours600Action();
 				}
 			}
@@ -105,22 +113,61 @@ class Requirements {
 		if(this.requiredCourses[course] !== undefined) {
 			this.requiredCourses[course] = true;
 			if(this.courseActions[course] !== undefined) {
-				this.courseActions[course]();
+				this.courseActions[course](true);
 			}
 		}
 		
 		if(this.hoursAction) {
-			this.hoursAction(hours);
+			this.hoursAction(this.currentHours);
 		}
 		if(!this.fufilledNotification && this.fufillsRequirements()) {
 			this.fufilledNotification = true;
-			this.fufilledOperation();
+			this.fufilledOperation(true);
 		}
 	}
 	
+	// TODO check for flagged courses that were invalid but may be valid now after an hour change
 	removeCourse(course, hours) {
 		console.log("remove course event, course is " + course + " with hours " + hours);
-		this.hoursAction(-hours);
+		var catalogEntry = this.coursesTaken[course];
+		this.currentHours -= hours;
+		this.hoursAction(this.currentHours);
+		if(this.requiredCourses[course] !== undefined) {
+			this.requiredCourses[course] = false;
+			if(this.courseActions[course] !== undefined) {
+				this.courseActions[course](false);
+			}
+		}
+		if(catalogEntry.number < 500) {
+			this.currentHours400 -= hours;
+			if(this.hours400Notification) {
+				var hourCap = (this.requiredHours > this.currentHours) ? (Math.ceil(this.requiredHours * this.hours400)) : (Math.ceil(this.currentHours * this.hours400));
+				if(this.currentHours400 <= hourCap) {
+					this.hours400Notification = false;
+				}
+			}
+		} else if(catalogEntry.number < 600) {
+			this.currentHours500 -= hours;
+			if(this.hours500Notification) {
+				var hourCap = (this.requiredHours > this.currentHours) ? (Math.ceil(this.requiredHours * this.hours500)) : (Math.ceil(this.currentHours * this.hours500));
+				if(this.currentHours500 <= hourCap) {
+					this.hours500Notification = false;
+				}
+			}
+		} else {
+			this.currentHours600 -= hours;
+			if(this.hours600Notification) {
+				var hourCap = (this.requiredHours > this.currentHours) ? (Math.ceil(this.requiredHours * this.hours600)) : (Math.ceil(this.currentHours * this.hours600));
+				if(this.currentHours600 <= hourCap) {
+					this.hours500Notification = false;
+				}
+			}
+		}
+		if(this.fufilledNotification && !this.fufillsRequirements()) {
+			this.fufilledNotification = false;
+			this.fufilledOperation(false);
+		}
+		this.coursesTaken[course] = null;
 	}
 	
 	setFufilledOperation(func) {
@@ -135,7 +182,7 @@ class Requirements {
 			if(!this.requiredCourses[key]) {
 				if(this.subCourses[key] !== undefined) {
 					var fufillsArray = this.subCourses[key].filter(function(courseId) {
-						return this.courses[courseId];
+						return this.requiredCourses[courseId];
 					}, this);
 					if(fufillsArray.length == 0) {
 						return false;
