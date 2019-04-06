59bf096d0484aca0195c1f9487df72d43f66e670
Casey Bates <cbates10@vols.utk.edu>
2019-04-06 14:39:25 -0500

    script to add a member to a committee

---
+++ scripts/addMemberToCommittee.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'DatabaseInfo.php';
+
+$con = new mysqli($servername, $sqlusername, $sqlpassword, $database);
+
+$con = mysqli_connect($servername, $sqlusername, $sqlpassword, $database);
+
+if($con->connect_errno) {
+	die("Failed to connect to MYSQL: ($mysql->connect_errno) $mysqli->connect_error");
+}
+
+$sql = "INSERT INTO committeemapping (subjectName, memberId, memberTitle) VALUES (? , ?, ?)";
+
+$stmt = $con->prepare($sql);
+if(!$stmt) {
+	die("Could not prepare statement $con->error");
+}
+if(!$stmt->bind_param("sis", $_POST["subject"], $_POST["id"], $_POST["title"])) {
+	die("Could not bind parameters $stmt->error");
+}
+$rc = $stmt->execute();
+if(false === $rc) {
+	die("Could not execute statement $stmt->error");
+}
+
+$stmt->close();
+$con->close();
+
+echo json_encode("success");
+
+?>
