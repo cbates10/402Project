a0591a9ec720bc570384598fab7fda564c028753
Casey Bates <cbates10@vols.utk.edu>
2019-04-09 20:08:44 -0500

    name changes

--- scripts/certificates.php
+++ scripts/certificates.php
@@ -12,19 +12,19 @@ $sql = "SELECT idObjects, name, subject FROM objects NATURAL JOIN types WHERE ty
 
 $result = $mysqli->query($sql);
 
-$minors = array();
+$certficates = array();
 
 if($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		$minors[$row["idObjects"]] = new stdClass();
-		$minors[$row["idObjects"]]->name = $row["name"];
-		$minors[$row["idObjects"]]->subject = $row["subject"];
+		$certficates[$row["idObjects"]] = new stdClass();
+		$certficates[$row["idObjects"]]->name = $row["name"];
+		$certficates[$row["idObjects"]]->subject = $row["subject"];
 	}
 }
 
 $mysqli->close();
 
-$minors = json_encode($minors);
-echo $minors;
+$certficates = json_encode($certficates);
+echo $certficates;
 
 ?>
