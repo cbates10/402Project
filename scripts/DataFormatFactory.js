6d5d3b34355cb435cb65882de6a9a0993b429486
Casey Bates <cbates10@vols.utk.edu>
2019-04-10 21:39:44 -0500

    change in formatting. Now accounts for violation flags as well as other formatting such as committee members

--- scripts/DataFormatFactory.js
+++ scripts/DataFormatFactory.js
@@ -1,7 +1,27 @@
 class DataFormatFactory {
 	
-	static formatData(programLevel, requirements) {
+	static getCommitteeMembers(subject, callback) {
+		$.ajax({
+			type: "POST",
+			url: "../scripts/getCommitteeMembersBySubject.php",
+			data: { committeeName: subject },
+			dataType: "json",
+			async: false,
+			success: function(json) {
+				callback(json);
+			},
+			error: function(xhr, ajaxOptions, thrownError) {
+				console.log(thrownError);
+				callback({});
+			}
+		});
+	}
+	
+	static formatData(programLevel, subject, requirements, transferInstitution) {
+		this.transferInstitution = transferInstitution;
 		this.requirements = requirements;
+		this.subject = subject;
+		this.violatedRules = [];
 		if(programLevel === "Masters") {
 			return this.formatMastersData();
 		} else if(programLevel === "PhD") {
@@ -17,6 +37,12 @@ class DataFormatFactory {
 		data[fieldPrefix + "CourseNumber" + counter] = courseEntry.number;
 		data[fieldPrefix + "CourseTitle" + counter] = courseEntry.name;
 		if(courseEntry.flags.length > 0) {
+			console.log(courseEntry.flags);
+			for(var i = 0; i < courseEntry.flags.length; i++) {
+				if(!this.violatedRules.includes(courseEntry.flags[i])) {
+					this.violatedRules.push(courseEntry.flags[i]);
+				}
+			}
 			data[fieldPrefix + "Flag" + counter] = "*";
 		}
 		return data;
@@ -25,12 +51,26 @@ class DataFormatFactory {
 	static formatCookieInformation(data) {
 		data["FirstName"] = getCookie("firstName");
 		data["LastName"] = getCookie("lastName");
+		data["StudentName"] = data["FirstName"] + " " + data["LastName"];
 		data["MiddleName"] = getCookie("middleName");
 		data["Email"] = getCookie("email");
 		data["StudentID"] = getCookie("studentID");
+		data["StudentIDpage2"] = data["StudentID"];
 		data["Major"] = getCookie("program");
 		data["ThesisType"] = getCookie("option");
 		data["FormName"] = requirements["Form"][0].name;
+		return data;
+	}
+	
+	static formatFlagLegend(data) {
+		console.log(this.violatedRules);
+		if(this.violatedRules.length > 0) {
+			data["FlagLegend"] = "The Following Hour Rules Are Violated On This Form: ";
+			data["FlagLegend1"] = this.violatedRules[0] + " ";
+		}
+		for(var i = 1; i < this.violatedRules.length; i++) {
+			data["FlagLegend1"] += this.violatedRules[i] + " ";
+		}
 		return data;
 	}
 	
@@ -58,15 +98,49 @@ class DataFormatFactory {
 		var minor = this.requirements.find(function(requirement) {
 			return requirement.getRequirementType() == requirementType.MINOR && requirement.fufillsRequirements();
 		});
-		if(!minor) {
-			return data;
+		if(minor) {
+			appliedCourses = minor.getAppliedCourses();
+			data["Minor"] = minor.getName();
+			courseCounter = 1;
+			Object.keys(appliedCourses).forEach(function(courseId) {
+				data = this.formatMajorData(data, courseCounter, "Minor", appliedCourses[courseId]);
+				courseCounter++;
+			}, this);
 		}
-		appliedCourses = minor.getAppliedCourses();
-		courseCounter = 1;
-		Object.keys(appliedCourses).forEach(function(courseId) {
-			data = this.formatMajorData(data, courseCounter, "Minor", appliedCourses[courseId]);
-			courseCounter++;
-		}, this);
+		this.getCommitteeMembers(this.subject, function(members) {
+			var chairMembers = [];
+			var directorMembers = [];
+			var committeeMembers = [];
+			for(var index in members) {
+				if(members[index].title == "Director") {
+					directorMembers.push(members[index].firstName + " " + members[index].lastName);
+					committeeMembers.push(members[index].firstName + " " + members[index].lastName);
+				} else if(members[index].title == "Member") {
+					committeeMembers.push(members[index].firstName + " " + members[index].lastName);
+				} else if(members[index].title == "Chairperson") {
+					chairMembers.push(members[index].firstName + " " + members[index].lastName);
+				}
+			}
+			if(committeeMembers.length < 3) {
+				data["Committee1"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+				data["Committee2"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+				data["Committee3"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+			} else {
+				data["Committee1"] = committeeMembers[0];
+				data["Committee2"] = committeeMembers[1];
+				data["Committee3"] = committeeMembers[2];
+			}
+			if(directorMembers.length > 0) {
+				data["GraduateProgramDirector"] = directorMembers[0];
+			}
+			if(chairMembers.length > 0) {
+				data["ChairPerson"] = chairMembers[0];
+			}
+		});
+		if(this.transferInstitution) {
+			data["TransferInstitution"] = this.transferInstitution;
+		}
+		data = this.formatFlagLegend(data)
 		return data;
 	}
 	
@@ -89,6 +163,44 @@ class DataFormatFactory {
 				courseCounterPhD++;
 			}
 		}, this);
+		this.getCommitteeMembers(this.subject, function(members) {
+			var chairMembers = [];
+			var directorMembers = [];
+			var committeeMembers = [];
+			for(var index in members) {
+				if(members[index].title == "Director") {
+					directorMembers.push(members[index].firstName + " " + members[index].lastName);
+					committeeMembers.push(members[index].firstName + " " + members[index].lastName);
+				} else if(members[index].title == "Member") {
+					committeeMembers.push(members[index].firstName + " " + members[index].lastName);
+				} else if(members[index].title == "Chairperson") {
+					chairMembers.push(members[index].firstName + " " + members[index].lastName);
+				}
+			}
+			if(committeeMembers.length < 5) {
+				data["Committee1"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+				data["Committee2"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+				data["Committee3"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+				data["Committee4"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+				data["Committee5"] = committeeMembers[Math.floor(Math.random() * committeeMembers.length)];
+			} else {
+				data["Committee1"] = committeeMembers[0];
+				data["Committee2"] = committeeMembers[1];
+				data["Committee3"] = committeeMembers[2];
+				data["Committee4"] = committeeMembers[3];
+				data["Committee5"] = committeeMembers[4];
+			}
+			if(directorMembers.length > 0) {
+				data["GraduateProgramDirector"] = directorMembers[0];
+			}
+			if(chairMembers.length > 0) {
+				data["ChairPerson"] = chairMembers[0];
+			}
+		});
+		if(this.transferInstitution) {
+			data["TransferInstitution"] = this.transferInstitution;
+		}
+		data = this.formatFlagLegend(data)
 		return data;
 	}
 }
